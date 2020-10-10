b41a2e161478f6273decaf5bb6654fef47994871
mac-book <lexx.mg@gmail.com>
2020-10-10 15:53:45 +0300

    edit js

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 'ues strict';
 
-const formRadio = document.querySelectorAll('.form__input-radio'),
-      formFiz = document.querySelector('.card-fiz__form');
+const formFiz = document.querySelector('.card-fiz__form');
 
 formFiz.addEventListener('click', function(event) {
-  for (let i = 0; i < formRadio.length; i++) {
-    if (formRadio[i].checked) {
-      formRadio[i].nextElementSibling.nextElementSibling.disabled = true;
-      formRadio[i].nextElementSibling.nextElementSibling.value = '';
+  const radio = this.querySelectorAll('input[type="radio"]');
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked) {
+      radio[i].nextElementSibling.nextElementSibling.disabled = true;
+      radio[i].nextElementSibling.nextElementSibling.value = '';
     } else {
-      formRadio[i].nextElementSibling.nextElementSibling.disabled = false;
+      radio[i].nextElementSibling.nextElementSibling.disabled = false;
     }
   }
 });
 
 formFiz.addEventListener('submit', function() {
   event.preventDefault();
-  if (formRadio[2].checked) {
+  if (this.querySelector('#salt-radio').checked) {
     const result = (this.inputWater.value / 100) * this.inputPer.value;
     this.inputSalt.value = result;
   }
 
-  if (formRadio[0].checked) {
+  if (this.querySelector('#per-radio').checked) {
     const result = (100 / this.inputWater.value) * this.inputSalt.value;
     this.inputPer.value = result;
   }
 
-  if (formRadio[1].checked) {
+  if (this.querySelector('#water-radio').checked) {
     const result = this.inputSalt.value * 100 / this.inputPer.value;
     this.inputWater.value = result;
   }
