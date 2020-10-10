84439c87eb14ef5eea4e3104339ab2b1ff14a0ad
mac-book <lexx.mg@gmail.com>
2020-10-10 19:33:38 +0300

    sync

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
 'ues strict';
 
-const formFiz = document.querySelector('.card-fiz__form');
+const formFiz = document.querySelector('.form-fiz-js'),
+      formAlc = document.querySelector('.form-alc-js');
 
 formFiz.addEventListener('click', function(event) {
+  const radio = this.querySelectorAll('input[type="radio"]');
+  for (let i = 0; i < radio.length; i++) {
+    if (radio[i].checked) {
+      radio[i].nextElementSibling.nextElementSibling.disabled = true;
+      if (!radio[i].nextElementSibling.nextElementSibling.dataset.on) {
+        radio[i].nextElementSibling.nextElementSibling.value = '';
+      }
+    } else {
+      radio[i].nextElementSibling.nextElementSibling.disabled = false;
+      radio[i].nextElementSibling.nextElementSibling.removeAttribute('data-on');
+    }
+  }
+});
+
+formAlc.addEventListener('click', function(event) {
   const radio = this.querySelectorAll('input[type="radio"]');
   for (let i = 0; i < radio.length; i++) {
     if (radio[i].checked) {
@@ -35,5 +51,32 @@ formFiz.addEventListener('submit', function() {
     const result = this.inputSalt.value * 100 / this.inputPer.value;
     this.inputWater.value = result;
     this.inputWater.dataset.on = 'on';
+  }
+});
+
+formAlc.addEventListener('submit', function() {
+  event.preventDefault();
+  if (this.querySelector('#Alc-radio').checked) {
+    const result = (this.inputWaterAlc.value / 100) * this.inputPerAlc.value;
+    this.inputAlc.value = result;
+    this.inputAlc.dataset.on = 'on';
+  }
+
+  if (this.querySelector('#perAlc-radio').checked) {
+    const result = (100 / this.inputWaterAlc.value) * this.inputAlc.value;
+    this.inputPerAlc.value = result;
+    this.inputPerAlc.dataset.on = 'on';
+  }
+
+  if (this.querySelector('#waterAlc-radio').checked) {
+    const result = this.inputAlc.value * 100 / this.inputPerAlc.value;
+    this.inputWaterAlc.value = result;
+    this.inputWaterAlc.dataset.on = 'on';
+  }
+
+  if (this.querySelector('#volAlc-input').checked) {
+    const result = this.inputAlc.value * 100 / this.inputPerAlc.value;
+    // this.inputVol.value = result;
+    // this.inputVol.dataset.on = 'on';
   }
 });
